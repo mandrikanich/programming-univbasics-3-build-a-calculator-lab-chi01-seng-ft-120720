0f7bbcb9ad8cabbd7d5081118ca326902ca882b0
Michael Andrikanich <mandrikanich@gmail.com>
2020-10-21 15:09:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,11 +6,11 @@ def subtraction(num1, num2)
   num2 - num1
 end
 
-def division(num1, num2)
+def multiplication(num1, num2)
    num1 * num2
 end
 
-def multiplication(num1, num2)
+def division(num1, num2)
   num1 \ num2
 end
 
